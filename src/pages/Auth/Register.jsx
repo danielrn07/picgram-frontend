e075186fa98ca985ae905cb6cb32d1ec8388f4a3
Daniel Nascimento <pdaniel.contato@gmail.com>
2024-07-15 21:01:07 -0300

    feat: add http requests configuration

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,5 +1,62 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { AuthContainer, RegisterContent, Subtitle, Title } from './styles'
+
 const Register = () => {
-  return <h1>Register</h1>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    }
+  }
+
+  return (
+    <AuthContainer>
+      <RegisterContent>
+        <Title>PicGram</Title>
+        <Subtitle>Cadastre-se para ver fotos e vídeos dos seus amigos.</Subtitle>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Nome'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <input
+            type='email'
+            placeholder='E-mail'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <input
+            type='password'
+            placeholder='Senha'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <input
+            type='password'
+            placeholder='Confirme a senha'
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={confirmPassword}
+          />
+          <button type='submit'>Cadastrar</button>
+        </form>
+        <div className='auth-panel'>
+          <p>Tem uma conta?</p>
+          <Link to='/login'>Conecte-se</Link>
+        </div>
+      </RegisterContent>
+    </AuthContainer>
+  )
 }
 
 export default Register
---
+++ src/pages/Auth/styles.jsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components'
+
+export const AuthContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  flex-grow: 1;
+
+  div {
+    form {
+      display: flex;
+      flex-direction: column;
+      gap: 0.8rem;
+    }
+
+    form input {
+      padding: 1rem;
+      min-width: 300px;
+    }
+
+    form button {
+      min-width: 100%;
+    }
+
+    .auth-panel {
+      display: flex;
+      gap: 0.8rem;
+      border-top: 1px solid #ccc;
+      width: 80%;
+      justify-content: center;
+      padding-top: 3.2rem;
+    }
+
+    .auth-panel a {
+      color: rgb(0, 149, 246);
+      font-weight: bold;
+    }
+  }
+`
+
+export const RegisterContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1.4rem;
+  gap: 3.2rem;
+  border: 1px solid rgb(230, 230, 230);
+  padding: 3.2rem 0;
+`
+
+export const Title = styled.h1`
+  font-size: 3.2rem;
+  font-weight: bold;
+`
+
+export const Subtitle = styled.h2`
+  max-width: 60%;
+  text-align: center;
+  color: rgb(115, 115, 115);
+  font-weight: bold;
+  font-size: 1.6rem;
+`
